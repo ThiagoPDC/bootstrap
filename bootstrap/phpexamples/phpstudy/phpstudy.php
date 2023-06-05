0c6991451c4cea8cd5f183b73bc8f1578f902e25
ThiagoPDC <thiagopavandallacosta@gmail.com>
2023-06-05 16:49:43 -0300

    aula05/06

--- bootstrap/phpexamples/phpstudy/phpstudy.php
+++ bootstrap/phpexamples/phpstudy/phpstudy.php
@@ -11,162 +11,31 @@
 <body>
 
 <?php include("../../menu.php")  ?>
-<div class="container mt-5 just" >
-  <div class="row just">
-  <div class="col-sm-3 card bg-dark m-2" id="constituicao1934">
-     <div class="card-header"> <h3>Constituição 1934</h3></div>
-      
-     <div class="card-body"><p>- ampliação do Poder Executivo
-      <ul>
-        <li>voto obrigatório e secreto(maiores de 18 anos, inclusive mulheres excluídos mendigos e analfabetos)</li>
-        <li><br>Criação da Justiça Eleitoral e da justiça do trabalho</li>
-        <li><br>Jornada de trabalho de 8 horas, repouso semanal e férias remuneradas</li>
-      </ul>
-    </p>
-    </div>
-  </div>
-    <div class="col-sm-3 card bg-dark m-2" id="constituicao1937">
-      <div class="card-header"> <h3>Constituição 1937 (outorgada)</h3></div>     
-        <div class="card-body">
-        <p>
-        <ul>
-          <li>Supressão do poder dos Partidos Políticos</li>
-        <br><li>Concentração de Poder no executivo</li>
-        <br><li>Fim da librdade de imprensa</li>
-        <br><li>Anulação da indepêmncia dos poderes</li>
-        <br><li>Suspensão da Imunidade Parlamentar</li>
-        <br><li>pena de morte, Prisão e exílio de opositores</li>
-        <br><li>Eleição indireta e mandato presidencial de 6 anos</li>
-        </ul>
-      </p>
-      </div>
-    </div>
-    <div>
-      <hr>
-        <h3>Quarta República</h3>
-      <hr>
-    </div>
-    <div class="col-sm-3 card bg-dark m-2" id="constituicao1946">
-      <div class="card-header"> <h3>Constituição 1946</h3></div>     
-        <div class="card-body">
-        <p>
-        <ul>
-          <br><li>Restabelecimento dos direitos individuais, fim da censura e da pena de morte</li>
-          <br><li>indepêmncia dos 3 poderes</li>
-          <br><li>Liberdade de organização partidária</li>
-          <br><li>Autonomia para estados e municípios</li>
-          <br><li>Eleição direta e mandato presidencial de 5 anos</li>
-          <br><li>Direito de greve e liberdade sindical</li>
-          <br><li></li>
-        </ul>
-      </p>
-      </div>
-    </div>
-    <div>
-      <hr>
-        <h3>Partidos Políticos</h3>
-      <hr>
-    </div>
-    <div class="col-sm-3 card bg-dark m-2" id="União Democrática Nacional">
-      <div class="card-header"> <h3>União Democrática Nacional - UDN </h3></div>     
-        <div class="card-body">
-        <p>
-        <ul>
-          <br><li>(Direita) Liberal na economia, conservador nos costumes</li>
-          <br><li>Pauta moralista anticorrupção</li>
-          <br><li>Antigetulismo</li>
-          <br><li>Golpismo e instabilização do regime democráttico</li>
-          <br><li>Líder : Carlos Lacerda</li>
-        </ul>
-      </p>
-      </div>
-    </div>
-    <div class="col-sm-3 card bg-dark m-2" id="Partido Social Democrata">
-      <div class="card-header"> <h3>Partido Social Democrata - PSD </h3></div>     
-        <div class="card-body">
-        <p>
-        <ul>
-          <br><li>(Centro) Formado por Burocratas, Getulistas (ex-intervventores)</li>
-          <br><li>Maior partido do período</li>
-          <br><li>Líder : Juscelino Kubitschek</li>
-        </ul>
-      </p>
-      </div>
-    </div>
-    <div class="col-sm-3 card bg-dark m-2" id="Partido Trabalhista Brasileiro">
-      <div class="card-header"> <h3>Partido Trabalhista Brasileiro - PTB </h3></div>     
-        <div class="card-body">
-        <p>
-        <ul>
-          <br><li>(Esquerda) Partido criado por Getúlio, ligado aos princípios sindicais do Brasil (apelo junto aos trabalhadores urbanos)</li>
-          <br><li>Defendia as reformas de Base (Agrária, Fiscal,Urbana ,etc) como método para reduzir a desigualdade social e garantir o desenvolvimento econômico</li>
-          <br><li></li>
-        </ul> 
-      </p>
-      </div>
-    </div>
-    <div class="col-sm-3 card bg-dark m-2" id="Partido Comunista do Brasil">
-      <div class="card-header"> <h3>Partido comunista do Brasil - PCB </h3></div>     
-        <div class="card-body">
-        <p>
-        <ul>
-          <br><li>(Esquerda) Legalizado em 1945 e tornado novamente ilegal pelo governo Dutra em 1947</li>
-          <br><li>Continuou atuando na ilegalidade junto aos sindicatos, trabalhadores urbanos e rurais</li>
-        </ul> 
-      </p>
-      </div>
-    </div>
-    <div>
-      <hr>
-        <h3>Presidentes 1946-1964</h3>
-      <hr>
-    </div>
-    <div class="col-sm-4 card bg-dark m-2" id="Presidentes">
-      <div class="card-header"> <h3>Presidentes</h3></div>     
-        <div class="card-body">
-        <p>
-        <ul>
-          <li>
-            <sb>Eurico Gaspar Dutra(1946-1950) PSD</sb>
-              <ul>
-                <li></li>
-              </ul>
-          </li>
 
-          <li>
-            <sb>Getulio Vargas (1951-1954) PTB</sb></li>
-              <ul>
-                <li>Criação da Petrobrás</li>
-              </ul>
-          <li>
-            <sb>Café Filho (1954-1955)</sb>
-              <ul>
-                <li></li>
-              </ul>
-          </li>
-          <li>
-            <sb>Juscelino Kubitschek (1955-1960) PSD</sb>
-              <ul>
-                <li></li>
-              </ul>
-          </li>
-          <li>
-            <sb>Jânio Quadros (1960-1961) UDN</sb>
-              <ul>
-                <li></li>
-              </ul>
-          </li>
-          <li>
-            <sb>João Goulart (1961-1964) PTB</sb>
-              <ul>
-                <li></li>
-              </ul>
-          </li>
-        </ul>
-      </p>
-      </div>
-    </div>
+ <?php
+    $dir = 'C:\xampp\htdocs\xampp\bootstrap\bootstrap\img';
+    $files = scandir($dir);
+    
+  ?>
+<div class="container mt-5 just" >
+  <div class="row">
+    <?php
+      foreach($files as $file){
+        if($file!='.' && $file!='..'){
+          echo "
+            <div class='col-sm-3 card bg-dark m-2'>
+              <div class='card-header'> <h3>$file</h3></div>
+              <div class='card-body'>
+                <img src='/bootstrap/img/$file'>
+              </div>
+            </div>
+          ";
+        }
+      }
+    ?>
   </div>
 </div>
+
+
 </body>
 </html>
